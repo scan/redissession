fdce1bd7f2c5f368fdcf4e4797c569fb9d6928b5
Scán <dusklykan@yahoo.co.uk>
2012-03-14 15:48:47 +0100

    simple session store

--- Web/RedisSession.hs
+++ Web/RedisSession.hs
@@ -1,8 +1,28 @@
 module Web.RedisSession where
 
 import Database.Redis.Redis
+import Data.ByteString (ByteString)
+import Data.Binary (decode, encode, Binary)
 
 withRedis server port f = do
 	redis <- connect server port
 	f redis
 	disconnect redis
+
+withRedisLocal f = do
+	redis <- connect localhost defaultPort
+	f redis
+	disconnect redis
+
+setSession :: (Binary a) => ByteString -> a -> Redis -> IO ()
+setSession key value redis = do
+	set redis key $ encode value
+	return ()
+
+getSession :: (Binary a) => ByteString -> Redis -> IO (Maybe a)
+getSession key redis = do
+	val <- get redis key
+	case val of
+		RBulk Nothing -> return Nothing
+		RBulk (Just v) -> return $ Just $ decode v
+		_ -> return Nothing
